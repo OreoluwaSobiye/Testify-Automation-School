5834071941ee2491679409a771f641a6167e6f90
Oreoluwa Sobiye <115700470+OreoluwaSobiye@users.noreply.github.com>
2026-07-04 21:15:35 +0100

    Task 2 Module 4a

---
+++ Module 4a Java Script/Tasks/Task 2.js
@@ -0,0 +1,16 @@
+/*
+1. No relation to Java:
+The name "JavaScript" was purely a marketing tactic to leverage the massive popularity of Sun Microsystems' Java language at the time
+
+2. Backward compatibility: 
+JavaScript is 100% backward compatible, meaning code written for a browser in 1995 still runs perfectly today.
+
+3. Runs outside browsers: 
+Thanks to runtime environments like Node.js, JavaScript is widely used for building backend servers, games, and mobile apps.
+
+4. Invisible semicolons: 
+The interpreter automatically finishes your lines using Automatic Semicolon Insertion (ASI), meaning semicolons are usually optional.
+
+5. The Banana joke: 
+Typing ('b' + 'a' + + 'a' + 'a').toLowerCase() outputs "banana" because the standalone + 'a' attempts to convert the string into a number, resulting in NaN
+*/
